2ddfe0288fde0334b40521ce5df2b045fd138fd1
ADangerousMix <ned@adangerousmix.com>
2021-03-07 00:58:59 -0500

    Testing.

--- widget.js
+++ widget.js
@@ -1,9 +1,17 @@
+const container = document.getElementsByClassName("main-container")[0];
+
+let donations = [];
+
 window.addEventListener("onWidgetLoad", async (obj) => {
 
 });
 
 window.addEventListener("onEventReceived", (obj) => {
-    
+    const event = obj.detail.event;
+  
+    if (event.listener === "widget-button" && event.field === "testAlert") {
+        playAlert();
+    }
 });
 
 window.addEventListener("onSessionUpdate", (obj) => {
