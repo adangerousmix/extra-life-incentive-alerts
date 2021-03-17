132b43c963258cfe63496e88655fefa59813ca24
ADangerousMix <ned@adangerousmix.com>
2021-03-17 10:37:06 -0400

    Update widget.

--- widget.js
+++ widget.js
@@ -21,7 +21,7 @@ let playAlert = () => {
             alert.classList.add("alert-hide");
             void alert.offsetWidth;
             alert.classList.remove("alert-show");
-        }, 3000);
+        }, 6000);
     }
 };
 
